60a1830016617ee0c8d57014fac8c7f369c77d05
Manishselvam <manishselvam06@gmail.com>
2025-12-23 19:08:33 +0530

    Day 7: Enhanced Number Guessing Game

--- number_guessing.py
+++ number_guessing.py
@@ -1,9 +1,10 @@
 import random
 
-number = random.randint(1, 10)
+number = random.randint(1, 100)
 attempts = 0
-while(True):
-    guess=int(input("Guess a number between 1 and 10:"))
+max_attempts = 10
+while attempts<max_attempts:
+    guess=int(input("Guess a number between 1 and 100:"))
     attempts+=1
     if guess==number:
         print("Correct!You guessed the number")
@@ -13,3 +14,7 @@ while(True):
         print("Too low!try again.")
     else:
         print("Too high!Try again.")
+    play_again=input("Do you want to play again? (yes/no):").lower()
+    if play_again!="yes":
+        print("Thanks for playing")
+        break
