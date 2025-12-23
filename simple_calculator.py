2dc721641d94abd574e3c9587b2b798f919e6541
Manishselvam <manishselvam06@gmail.com>
2025-12-23 13:55:59 +0530

    Day 6: Simple Calculator project

---
+++ simple_calculator.py
@@ -0,0 +1,16 @@
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+operation=input("Enter operation (+, -, *, /): ")
+if operation=='+':
+    print("add:",num1+num2)
+elif operation=='-':
+    print("sub:",num1-num2)
+elif operation=='*':
+    print("mul:",num1*num2)
+elif operation=='/':
+    if num2!=0:
+        print("div:",num1/num2)
+    else:
+        print("Error: Division by zero")
+else:
+    print("Invalid operation")
